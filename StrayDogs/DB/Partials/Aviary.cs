3d0371ddc50d03457c2857e9a413be6171f69f33
RuVavava <varya.kornilova.21@inbox.ru>
2025-02-12 15:10:17 +0300

    .

---
+++ StrayDogs/DB/Partials/Aviary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrayDogs.DB
+{
+    public partial class Aviary
+    {
+        public string Status
+        {
+            get
+            {
+                string Status1 = 'Занят';
+                string Status2 = 'Свободен';
+
+                var status = DBConnection.stray_DogsEntities.Aviary.Any(i => i.Id == dddd).ToList();
+            }
+        }
+    }
+}
